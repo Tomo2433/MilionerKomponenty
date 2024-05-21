a307e846542ecfc994ed6629692d2d4b1cd6798f
byeolpyo <byeolpyo@protonmail.com>
2024-05-21 16:25:23 +0200

    rewards

--- MilionerKomponenty/IGameState.cs
+++ MilionerKomponenty/IGameState.cs
@@ -10,12 +10,25 @@ using System.Threading.Tasks;
 
 namespace MilionerKomponenty
 {
+    public enum GameProgress {
+        GAME_IN_PROGRESS,
+        GAME_OVER_WIN,
+        GAME_OVER_LOSS
+    };
     public interface IGameState
     {
+
         string GetQuestion();
         List<string> GetAnswers();
         string GetCorrectAnswer();
         bool IsGenerating();
         Task FetchAndUpdate();
+
+        void ResetGame();
+
+        void PlayerResponded(bool is_response_correct);
+        GameProgress GetGameProgress();
+        int GetCurrentReward();
+
     }
 }
