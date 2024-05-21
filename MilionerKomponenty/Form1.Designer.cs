259b21a90abb5852c4bc98fc9f9b0b548566d5a5
Tomo2433 <tomaszlech56@gmail.com>
2024-05-21 14:09:22 +0200

    interface update

--- MilionerKomponenty/Form1.Designer.cs
+++ MilionerKomponenty/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.question;
+            pictureBox1.Location = new Point(-16, 194);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(818, 110);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.MidnightBlue;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.ForeColor = SystemColors.Window;
+            textBox1.Location = new Point(122, 207);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(550, 81);
+            textBox1.TabIndex = 1;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.UseWaitCursor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = Properties.Resources.answers;
+            pictureBox2.Location = new Point(-22, 302);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(824, 61);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Image = Properties.Resources.answers;
+            pictureBox3.Location = new Point(-24, 361);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(826, 61);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Navy;
+            button1.FlatAppearance.BorderColor = Color.Navy;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(94, 315);
+            button1.Name = "button1";
+            button1.Size = new Size(276, 36);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Navy;
+            button2.FlatAppearance.BorderColor = Color.Navy;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(94, 374);
+            button2.Name = "button2";
+            button2.Size = new Size(276, 36);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Navy;
+            button3.FlatAppearance.BorderColor = Color.Navy;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(412, 315);
+            button3.Name = "button3";
+            button3.Size = new Size(276, 36);
+            button3.TabIndex = 6;
+            button3.Text = "button3";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Navy;
+            button4.FlatAppearance.BorderColor = Color.Navy;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(412, 374);
+            button4.Name = "button4";
+            button4.Size = new Size(276, 36);
+            button4.TabIndex = 7;
+            button4.Text = "button4";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
