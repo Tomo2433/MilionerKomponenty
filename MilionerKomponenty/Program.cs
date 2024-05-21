83335962b132b8efd64c01f406195d987a587a86
Tomo2433 <tomaszlech56@gmail.com>
2024-05-21 14:41:30 +0200

    question list panel

--- MilionerKomponenty/Program.cs
+++ MilionerKomponenty/Program.cs
@@ -10,20 +10,20 @@ namespace MilionerKomponenty
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1());
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
 
-            var x = new DummyGenerator();
-            x.SetSubject("world war");
-            x.SetAnswerCount(4);
-            x.Generate();
-            while (x.IsGenerating())
-            {
-                Console.WriteLine(".");
-                Thread.Sleep(100);
-            }
-            var res = x.FetchResponse();
-            x.ShowDebug();
+            //var x = new DummyGenerator();
+            //x.SetSubject("world war");
+            //x.SetAnswerCount(4);
+            //x.Generate();
+            //while (x.IsGenerating())
+            //{
+            //    Console.WriteLine(".");
+            //    Thread.Sleep(100);
+            //}
+            //var res = x.FetchResponse();
+            //x.ShowDebug();
 
         }
     }
