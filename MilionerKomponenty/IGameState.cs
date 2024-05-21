9e95c6f7cf0fca95f544f14d1da8f8e2fea30015
byeolpyo <byeolpyo@protonmail.com>
2024-05-21 15:02:56 +0200

    simple gamestate

---
+++ MilionerKomponenty/IGameState.cs
@@ -0,0 +1,21 @@
+// string pytanie
+// string[] odpowiedzi
+// string poprawna
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilionerKomponenty
+{
+    public interface IGameState
+    {
+        string GetQuestion();
+        List<string> GetAnswers();
+        string GetCorrectAnswer();
+
+        void FetchAndUpdate();
+    }
+}
